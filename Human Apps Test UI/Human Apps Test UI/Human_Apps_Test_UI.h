f040f2bb5c7ee5b6d6987bf0a84bab5a8bb18c40
MrSalei <ilia74311@yandex.by>
2024-08-11 19:06:23 +0300

    Project setup

---
+++ Human Apps Test UI/Human Apps Test UI/Human_Apps_Test_UI.h
@@ -0,0 +1,18 @@
+//
+//  Human_Apps_Test_UI.h
+//  Human Apps Test UI
+//
+//  Created by Илья Салей on 11.08.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Human_Apps_Test_UI.
+FOUNDATION_EXPORT double Human_Apps_Test_UIVersionNumber;
+
+//! Project version string for Human_Apps_Test_UI.
+FOUNDATION_EXPORT const unsigned char Human_Apps_Test_UIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Human_Apps_Test_UI/PublicHeader.h>
+
+
