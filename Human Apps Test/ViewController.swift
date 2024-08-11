d2de910459d2574d1ac52a360da36f732c6781f9
MrSalei <ilia74311@yandex.by>
2024-08-11 18:54:45 +0300

    Initial Commit

---
+++ Human Apps Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Human Apps Test
+//
+//  Created by Илья Салей on 11.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
